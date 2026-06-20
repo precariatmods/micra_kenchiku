e3e318bc2b07b14c97ee79cf3b6ddce5f189683b
gittest1 <test@aaaaa.aa>
2026-06-20 13:14:51 +0900

    0620

---
+++ tnthaichi.py
@@ -0,0 +1,52 @@
+from mctools import RCONClient
+import time
+
+mcr = RCONClient("192.168.1.10", port=25575)
+mcr.login("pass")
+
+def cmd(command):
+    print(command)
+    print(mcr.command(command))
+
+# TNT設置条件
+x = 338
+y = 109
+z_start = 150
+z_end = -50
+interval = 2
+
+# Z方向にTNTを設置
+for z in range(z_start, z_end - 1, -interval):
+    cmd(f"setblock {x} {y} {z} tnt")
+    time.sleep(0.05)
+
+cmd(f"setblock {x} {y} {z_end} tnt")
+
+# TNT設置条件
+x = x
+y = y+1
+z_start = z_start-1
+z_end = -50
+interval = 2
+
+# Z方向にTNTを設置
+for z in range(z_start, z_end - 1, -interval):
+    cmd(f"setblock {x} {y} {z} tnt")
+    time.sleep(0.05)
+
+cmd(f"setblock {x} {y} {z_end} tnt")
+
+
+# TNT設置条件
+x = x
+y = y+1
+z_start = z_start-1
+z_end = -50
+interval = 2
+
+# Z方向にTNTを設置
+for z in range(z_start, z_end - 1, -interval):
+    cmd(f"setblock {x} {y} {z} tnt")
+    time.sleep(0.05)
+
+cmd(f"setblock {x} {y} {z_end} tnt")
